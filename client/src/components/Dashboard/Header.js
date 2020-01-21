7f5d28ebbd7d3aa272c7ec81ba88d693ae22e1d4
Chase Laptop <james.chase.richards@gmail.com>
2020-01-21 13:08:18 -0500

    add group modal added with image upload supported by firebase

--- client/src/components/Dashboard/Header.js
+++ client/src/components/Dashboard/Header.js
@@ -1,24 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { CreateGroup } from "./CreateGroup";
 import Logo from "../../images/ActivityUP-Logo.png";
 // import LogoSVG from "../../../images/activityup.svg";
 
-function Header() {
-  return (
-    <header>
-      <Link to="/" className="logoContainer">
-        <img className="logo" src={Logo} alt="ActivityUP Logo"></img>
-      </Link>
-      <div className="navBtnContainer">
-        <Link to="#" className="newGroupBtn">
-          Create A New Group
-        </Link>
-        <Link to="/" className="loginBtn">
-          Logout
-        </Link>
-      </div>
-    </header>
-  );
+export class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { addModalShow: false };
+  }
+
+  render() {
+    let addModalClose = () => this.setState({ addModalShow: false });
+
+    return (
+      <>
+        <header>
+          <Link to="/" className="logoContainer">
+            <img className="logo" src={Logo} alt="ActivityUP Logo"></img>
+          </Link>
+          <div className="navBtnContainer">
+            <Link
+              className="newGroupBtn"
+              onClick={() => this.setState({ addModalShow: true })}
+            >
+              Create A New Group
+            </Link>
+            <Link to="/" className="loginBtn">
+              Logout
+            </Link>{" "}
+          </div>
+        </header>
+        <CreateGroup
+          show={this.state.addModalShow}
+          onHide={addModalClose}
+          style={{ background: "none" }}
+        />
+      </>
+    );
+  }
 }
 
 export default Header;
