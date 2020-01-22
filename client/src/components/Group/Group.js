368a6fcf9e10bb148e4947308b949554a750c6ce
Chase Laptop <james.chase.richards@gmail.com>
2020-01-22 12:01:19 -0500

    added calender to group page

--- client/src/components/Group/Group.js
+++ client/src/components/Group/Group.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Header from "./Header";
+import Calender from "./Calender";
 import Footer from "../Footer";
 
 function Group() {
   return (
     <div>
       <Header />
-      <div className="bodyContainer">
-        <h1>welcome to your group</h1>
-      </div>
+
+      <Calender />
+
       <Footer />
     </div>
   );
