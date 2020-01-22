ebd18b475a80fafd6bd309d9a97fa12c2f66778f
Jeffrey Mann <jwmann22@gmail.com>
2020-01-22 18:39:21 -0500

    acitvity type images generated more dynamically

--- client/src/components/Home/ActivityTypes.js
+++ client/src/components/Home/ActivityTypes.js
@@ -1,91 +1,22 @@
 import React from "react";
-import images from "../../utils/activityTypes.json"
+import images from "../../utils/activityTypes.json";
 
 function ActivityTypes() {
-  return (
-      <div className="activityTypesRow">
-        <div className="activityTypesColumn">
-          <img
-            className="activityTypesImage"
-            src={images.sports}
-            alt="sports"
-            width="100%"
-          />
-        </div>
-        <div className="activityTypesColumn">
-          <img
-            className="activityTypesImage"
-            src={images.drinks}
-            alt="drinks"
-            width="100%"
-          />
-        </div>
-        <div className="activityTypesColumn">
-          <img
-            className="activityTypesImage"
-            src={images.food}
-            alt="food"
-            width="100%"
-          />
-        </div>
-        <div className="activityTypesColumn">
-          <img
-            className="activityTypesImage"
-            src={images.outdoors}
-            alt="outdoors"
-            width="100%"
-          />
-        </div>
-        <div className="activityTypesColumn">
-          <img
-            className="activityTypesImage"
-            src={images.movies}
-            alt="movies"
-            width="100%"
-          />
-        </div>
-        <div className="activityTypesColumn">
-          <img
-            className="activityTypesImage"
-            src={images.professional}
-            alt="professional"
-            width="100%"
-          />
-        </div>
-        <div className="activityTypesColumn">
-          <img
-            className="activityTypesImage"
-            src={images.shopping}
-            alt="shopping"
-            width="100%"
-          />
-        </div>
-        <div className="activityTypesColumn">
-          <img
-            className="activityTypesImage"
-            src={images.music}
-            alt="music"
-            width="100%"
-          />
-        </div>
-        <div className="activityTypesColumn">
-          <img
-            className="activityTypesImage"
-            src={images.gaming}
-            alt="gaming"
-            width="100%"
-          />
-        </div>
-        <div className="activityTypesColumn">
-          <img
-            className="activityTypesImage"
-            src={images.sites}
-            alt="sites"
-            width="100%"
-          />
-        </div>
+  let activityImgs = [];
+  for (const key in images) {
+    activityImgs.push(
+      <div className="activityTypesColumn">
+        <img
+          src={images[key]}
+          alt={key}
+          width="100%"
+          className="activityTypesImage"
+        />
       </div>
-  );
+    );
+  }
+
+  return <div className="activityTypesRow">{activityImgs}</div>;
 }
 
 export default ActivityTypes;
