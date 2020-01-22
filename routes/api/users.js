0f9d375d9d0048c50a22f7e21646c8bfe894e1f5
Jeffrey Mann <jwmann22@gmail.com>
2020-01-22 15:41:57 -0500

    added POST /api/users/login/ path

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,24 @@
 const router = require("express").Router();
 const { userController } = require("../../controllers");
+const passport = require("../../passport");
 
 router
   .route("/")
   .get(userController.findAll)
   .post(userController.create);
 
-router
-  .route("/one/")
-  .get(userController.findOne)
+router.route("/one/").get(userController.findOne);
+
+router.route("/login").post(
+  function(req, res, next) {
+    console.log("LOGIN BODY", req.body);
+    next();
+  },
+  passport.authenticate("local"),
+  (req, res) => {
+    console.log("LOGGED IN", req.user, req.session);
+    res.send(req.user);
+  }
+);
 
 module.exports = router;
