71209844c3e1f2a221a70c2068b8592289e616d5
Jeffrey Mann <jwmann22@gmail.com>
2020-01-23 11:57:15 -0500

    registration works

--- controllers/users.js
+++ controllers/users.js
@@ -15,9 +15,33 @@ module.exports = {
     const newUser = new User({
       username: req.body.username,
       password: req.body.password
-    })
-    newUser.save()
+    });
+    newUser
+      .save()
       .then(data => res.json(data))
       .catch(err => res.status(422).json(err));
+  },
+  register: (req, res) => {
+    const { username, password } = req.body;
+
+    // ADD VALIDATION
+    User.findOne({ username: username }, (err, user) => {
+      if (err) {
+        console.log("User.js post error: ", err);
+      } else if (user) {
+        res.json({
+          error: `Sorry, already a user with the username: ${username}`
+        });
+      } else {
+        const newUser = new User({
+          username: username,
+          password: password
+        });
+        newUser.save((err, savedUser) => {
+          if (err) return res.json(err);
+          res.json(savedUser);
+        });
+      }
+    });
   }
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -7,30 +7,7 @@ router
   .get(userController.findAll)
   .post(userController.create);
 
-router.route("/register").post(() => {
-  console.log("user signup");
-
-  const { username, password } = req.body;
-  // ADD VALIDATION
-  User.findOne({ username: username }, (err, user) => {
-    if (err) {
-      console.log("User.js post error: ", err);
-    } else if (user) {
-      res.json({
-        error: `Sorry, already a user with the username: ${username}`
-      });
-    } else {
-      const newUser = new User({
-        username: username,
-        password: password
-      });
-      newUser.save((err, savedUser) => {
-        if (err) return res.json(err);
-        res.json(savedUser);
-      });
-    }
-  });
-});
+router.route("/register").post(userController.register);
 
 router.route("/login").post(
   function(req, res, next) {
@@ -40,7 +17,7 @@ router.route("/login").post(
   passport.authenticate("local"),
   (req, res) => {
     console.log("LOGGED IN", req.user, req.session);
-    res.send(req.user);
+    res.json(req.user._id);
   }
 );
 
