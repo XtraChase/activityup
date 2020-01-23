494e8f05de3391eeb6838607feb8a380f19a7b93
Chase Laptop <james.chase.richards@gmail.com>
2020-01-22 23:32:36 -0500

    text fix

--- client/src/components/Home/ActivityTypes.js
+++ client/src/components/Home/ActivityTypes.js
@@ -1,23 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import images from "../../utils/activityTypes.json";
 
-function ActivityTypes() {
-  let activityImgs = [];
-  for (const key in images) {
-    activityImgs.push(
-      <div className="activityTypesColumn" key={key + Date.now()}>
-        <img
-          src={images[key]}
-          alt={key}
-          width="100%"
-          className="activityTypesImage"
-        />
-        <p className="activityTypesText">{key.toUpperCase()}</p>
-      </div>
+export class ActivityTypes extends Component {
+  render() {
+    let activityImgs = [];
+
+    for (const key in images) {
+      activityImgs.push(
+        <div className="activityTypesColumn" key={key + Date.now()}>
+          <img
+            src={images[key]}
+            alt={key}
+            width="100%"
+            className="activityTypesImage"
+          />
+          <p className="activityTypesText">{key.toUpperCase()}</p>
+        </div>
+      );
+    }
+
+    return (
+      <>
+        <div className="activityTypesRow">{activityImgs}</div>
+      </>
     );
   }
-
-  return <div className="activityTypesRow">{activityImgs}</div>;
 }
 
 export default ActivityTypes;
