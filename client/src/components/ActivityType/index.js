75e0465833701897fab0cc1697e3c2313be5803c
Chase Laptop <james.chase.richards@gmail.com>
2020-01-27 23:46:36 -0500

    group event mapping

--- client/src/components/ActivityType/index.js
+++ client/src/components/ActivityType/index.js
@@ -5,29 +5,29 @@ import Footer from "../Footer";
 function ActivityType(props) {
   return (
     <div>
-      <Header authenticated={props.authenticated}/>
+      <Header authenticated={props.authenticated} />
       <div className="headerBuffer"></div>
       <div>
-      <div>
-        <h1>Your Activity</h1>
-        <div className="imageRow">
-          <div className="imageColumn">
-            <img
-              className="image"
-              src="https://s3.amazonaws.com/activejunky-cdn/aj-content/hikinggroup.jpg"
-              alt="activity type"
-              width="100%"
-            />
-            <div className="text-block">
-              <h4>Hiking at Apalaches</h4>
+        <div>
+          <h1>Outdoors</h1>
+          <div className="imageRow">
+            <div className="imageColumn">
+              <img
+                className="image"
+                src="https://s3.amazonaws.com/activejunky-cdn/aj-content/hikinggroup.jpg"
+                alt="activity type"
+                width="100%"
+              />
+              <div className="text-block">
+                <h4>Hiking at Appalachians</h4>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>     
       <Footer />
     </div>
   );
 }
 
-export default ActivityType ;
+export default ActivityType;
--- client/src/components/Group/GroupSuggestions.js
+++ client/src/components/Group/GroupSuggestions.js
@@ -1,79 +1,48 @@
-import React from "react";
-// import UpArrow from "../../images/Arrow.png";
+import React, { Component } from "react";
+import eventExample from "../../utils/eventExample.json";
+import UpVote from "../Home/UpVote";
 
-function GroupSuggestions() {
-  return (
-    <div>
-      <div>
+class GroupSuggestions extends Component {
+  state = {
+    eventExample,
+    event: "",
+    upvotes: 0
+  };
+
+  handleUpVote = activity => {
+    console.log("Activity Upvoted!");
+    // let update = {
+    //   upvotes: this.state.upvotes
+    // };
+  };
+
+  render() {
+    return (
+      <>
         <div className="imageRow">
-          <div className="imageColumn">
-            <img
-              className="image"
-              src="https://www.playstationlifestyle.net/assets/uploads/2020/01/Modern-Warfare-error.jpg"
-              alt="activity type"
-              width="100%"
-            />
-            <img
-              className="upVoteArrow"
-              src="/images/Arrow.png"
-              alt="Up Vote Arrow"
-              width="15px"
-              // onClick={this.state.UpVote()}
-            />
-            <p className="upVoteCount">5</p>
-          </div>
-          <div className="imageColumn">
-            <img
-              className="image"
-              src="https://hb.imgix.net/ae475dd426714e9e7857b28f92009011d16ac98e.jpg?auto=compress,format&fit=crop&h=353&w=616&s=ff11c567c3750076b8624c1f22a83547"
-              alt="activity type"
-              width="100%"
-            />
-            <img
-              className="upVoteArrow"
-              src="/images/Arrow.png"
-              alt="Up Vote Arrow"
-              width="15px"
-              // onClick={this.state.UpVote()}
-            />
-            <p className="upVoteCount">3</p>
-          </div>
-          <div className="imageColumn">
-            <img
-              className="image"
-              src="https://assets1.ignimgs.com/2018/02/23/overwatch-button-v2-1519416414955.jpg"
-              alt="activity type"
-              width="100%"
-            />
-            <img
-              className="upVoteArrow"
-              src="/images/Arrow.png"
-              alt="Up Vote Arrow"
-              width="15px"
-              // onClick={this.state.UpVote()}
-            />
-            <p className="upVoteCount">0</p>
-          </div>
-          <div className="imageColumn">
-            <img
-              className="image"
-              src="https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F85037341%2F26975821121%2F1%2Foriginal.20190305-175625?w=1000&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C50%2C1600%2C800&s=6ac1b61dd8f60ab58337213bb8794a09"
-              alt="activity type"
-              width="100%"
-            />
-            <img
-              className="upVoteArrow"
-              src="/images/Arrow.png"
-              alt="Up Vote Arrow"
-              width="15px"
-              // onClick={this.state.UpVote()}
-            />
-            <p className="upVoteCount">2</p>
-          </div>
+          {this.state.eventExample.map((e, i) => (
+            <div className="imageColumn">
+              <img
+                className="image"
+                width="100%"
+                key={e.event[i].id}
+                src={e.event[i].activities[i].image}
+                alt={e.event[i].date}
+              />
+              <UpVote
+                activity={e.event[i].id}
+                handleArrowClick={() => this.handleUpVote(e.event[i].id)}
+              />
+              <div className="text-block">
+                <h4>{e.event[i].activities[i].activity}</h4>
+                <p>{e.event[i].activities[i].subtitle}</p>
+              </div>
+            </div>
+          ))}
         </div>
-      </div>
-    </div>
-  );
+      </>
+    );
+  }
 }
 
 export default GroupSuggestions;
