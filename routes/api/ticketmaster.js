e1ef2a082cd06a118cb3358179d18b46623584ca
Cesar Sanchez-Vegas <cesar.sanchezvegas82@gmail.com>
2020-02-01 12:55:28 -0500

    added geekEvents proper searching

--- routes/api/ticketmaster.js
+++ routes/api/ticketmaster.js
@@ -11,7 +11,7 @@ router.route("/adevents").get(async (req, res) => {
 
 router.route("/geekevents").get(async (req, res) => {
   const events = await axios.get(
-"https://api.seatgeek.com/2/events?client_id=MjA1NDQ0MDR8MTU4MDU3NTc4MC4wOQ&client_secret=3bbf40538c93bcf935c817e740316407d450a6a2514173d76e0995bd7d50a674"  );
+`https://api.seatgeek.com/2/events?type=${req.query.type}&client_id=MjA1NDQ0MDR8MTU4MDU3NTc4MC4wOQ&client_secret=3bbf40538c93bcf935c817e740316407d450a6a2514173d76e0995bd7d50a674`  );
   console.log("events", events.data)
   res.json(events.data);
 });
