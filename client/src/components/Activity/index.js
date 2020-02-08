933f0f94d26e5661266e7e5340ad2f2cbeb848d7
Chase Laptop <james.chase.richards@gmail.com>
2020-02-08 16:56:42 -0500

    callback error fix

---
+++ client/src/components/Activity/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { ReactSVG } from "react-svg";
+
+function Activity(props) {
+  const {
+    category,
+    image,
+    activity,
+    upvotes,
+    // update,
+    subtitle,
+    getActivities,
+    style,
+    id
+  } = props;
+
+  return (
+    <>
+      <div className="imageColumn" category={category}>
+        <img
+          className="groupSuggestion"
+          width="100%"
+          id={id}
+          src={image}
+          alt={activity}
+        />
+        <ReactSVG
+          className="arrow"
+          src="images/Arrow.svg"
+          alt="Up Vote Arrow"
+          activity={activity}
+          onClick={() => {
+            getActivities(id);
+          }}
+          style={style}
+          // onChange={update}
+        />
+        <p className="upVoteCount">{upvotes}</p>
+        <div className="groupSuggestionTextBlock">
+          <h4>{activity}</h4>
+          <p>{subtitle}</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Activity;
